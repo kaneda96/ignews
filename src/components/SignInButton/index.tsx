5ecdef2b52fad101924d70ad7d8027d2603dc95a
Bruno Ferrari <bpetinelli@epharma.com.br>
2021-04-19 18:46:34 -0300

    adding login servers and database integration

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,31 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.SignInButton} type="button">
-      <FaGithub color="#04d361" /> Bruno Ferrari{" "}
+  return session ? (
+    <button
+      onClick={() => {
+        signOut();
+      }}
+      className={styles.SignInButton}
+      type="button"
+    >
+      <FaGithub color="#04d361" /> {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.SignInButton} type="button">
+    <button
+      onClick={() => {
+        signIn("github");
+      }}
+      className={styles.SignInButton}
+      type="button"
+    >
       <FaGithub color="#eba417" /> Sign In with Github
     </button>
   );
